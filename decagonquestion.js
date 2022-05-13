26d340d2e997ac5fe5c68702f3cf5933f085dcce
ibukunoluwa Akintobi <88123302+ibkishere@users.noreply.github.com>
2022-05-13 22:58:48 +0100

    Update decagonquestion.js

--- decagonquestion.js
+++ decagonquestion.js
@@ -18,7 +18,8 @@ function sumOfEvenAndOdd (generalArray) {
     " this is the sum of the oddArray " + reducedOddArray 
 }
 sumOfEvenAndOdd([3,2,4,8,5])
-// there is a console.log(print command) in the body, if we run the function, we dont need to use the console.log to print again but if we do, we are going to get an undefined value because it is similar to typing console.log(console.log()) so in this instance, if we want to use the return statement we will put both reduce expressions in the return statement or we can just create the variables withut adding console.log to them 
+// there is a console.log(print command) in the body, if we run the function, we dont need to use the console.log to print again but if we do, we are going to get an undefined value because it is similar to typing console.log(console.log()) so in this instance, if we want to use the return statement we wil
+//  put both reduce expressions in the return statement or we can just create the variables withut adding console.log to them 
 
 function sumOfEvenAndOdd (generalArray) {
     let evenArray = []
